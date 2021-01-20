733c4f73d3357f5c7e8340dd685c4f95eaa52290
Brian Cheng <bscheng@ECO-184175.local>
2021-01-20 11:29:01 -0500

    trouble shooting covariance matrix problem - SOLVED

--- R/03_Analysis2.R
+++ R/03_Analysis2.R
@@ -12,12 +12,12 @@ source(here('R', '00_Data_setup.R'))
 str(acc)
 
 acc<- acc %>% 
-  filter(n_1!=1 | n_2!=1)
+  filter(n_1!=1 | n_2!=1) #remove n = 1 studies bc no variation
 acc$study<-droplevels(acc$study)
 
 #first, error measurements are not all standard deviation (which metafor needs)
 acc<-acc %>% 
-  filter(thermal_limit_error_1!='NA') %>% #getting rid of 2 studies that do not have error estimate with thier thermal limit means
+  filter(thermal_limit_error_1!='NA') %>% #getting rid of 2 studies that do not have error estimate with their thermal limit means
   mutate(sd1i= case_when(thermal_limit_error_type=='CI' ~ (thermal_limit_error_1*sqrt(n_1))/1.96, 
                          thermal_limit_error_type=='std_err' ~ thermal_limit_error_1*sqrt(n_1),
                          thermal_limit_error_type=='std_dev' ~ thermal_limit_error_1), #converting error estimate for thermal_limit_error_1 to standard deviation
@@ -33,6 +33,14 @@ acc_es<- escalc(measure='SMD', m1i= thermal_limit_2, n1i=n_2, sd1i=sd2i, m2i=the
 #Thermal limit mean #2 is mean 1 in escalc to make a positive hedges g mean more plasticity
 acc_es<- as.data.frame(acc_es)
 str(acc_es)
+which(is.na(acc_es$yi))
+acc_es[336:342,] #thermal limits = 0, 0 variation
+acc_es[385:391,] #negative thermal limits, 0 variation
+acc_es[406,]     #negative thermal limits, 0 variation
+#getting rid of rows with NA in yi for now
+acc_es<- acc_es %>%   #remove NAs from 0 variation
+  filter(yi!='NA') 
+acc$study<-factor(acc$study)
 
 #trying to figure out data structure and NAs
 #acc_es$yi<-as.numeric(as.character(acc_es$yi))
@@ -43,6 +51,7 @@ str(acc_es)
   #print(n=56) %>% 
   #filter(n_1==1)
   
+acc_es$ni = unlist(lapply(split(acc_es, acc_es$study), function(x) rep(sum(x$n_1) + x$n_2[1], each=nrow(x)))) #this assumes common control, which is not what we are doing right now
 
 #now calculate variance/covariance matrix
 calc.v <- function(x) {
@@ -51,31 +60,84 @@ calc.v <- function(x) {
   v 
 }
 
-
-acc_es$ni = unlist(lapply(split(acc_es, acc_es$study), function(x) rep(sum(x$n_1) + x$n_2[1], each=nrow(x)))) #this assumes common control, which is not what we are doing right now
-
 #making an ni colum (sum of sample size for an entire study)
 acc_es<-acc_es %>% 
   group_by(study) %>% 
   mutate(ni= sum(n_1)+ sum(n_2)) 
 acc_es$ni<-as.numeric(as.character(acc_es$ni))
 str(acc_es)
+acc_es$study<-factor(acc_es$study)
+str(acc_es)
 
-#getting rid of rows with NA in yi for now
-#acc_es<-subset(acc_es, yi!='NA')
-#acc_es$study<-droplevels(acc_es$study)
-
+#write csv and read back in because tidy weirdness?
+write.csv(acc_es,file="acc_es.csv")
+acc_es_test<-read.csv(file="acc_es.csv",stringsAsFactors = T)
+str(acc_es_test)
 V<- bldiag(lapply(split(acc_es, acc_es$study), calc.v)) 
+all.equal(acc_es$vi, diag(V)) #check diagonals
+plot(acc_es$vi~diag(V))       
+
+#trial run with smaller subset of data (acc_es)
+acc_es2<-acc_es[c(1:343,351:538),]
+write.csv(acc_es2,file="acc_es2.csv")
+acc_small<-read.csv("acc_es2.csv", stringsAsFactors = T)
+V<- bldiag(lapply(split(acc_small, acc_small$study), calc.v)) 
+all.equal(acc_small$vi, diag(V)) #check diagonals
+plot(acc_small$vi~diag(V))       
+str(acc_small)
+
+#just jensen
+acc_es2<-acc_es[344:350,]
+write.csv(acc_es2,file="acc_es2.csv")
+acc_small<-read.csv("acc_es2.csv", stringsAsFactors = T)
+V<- bldiag(lapply(split(acc_small, acc_small$study), calc.v)) 
+all.equal(acc_small$vi, diag(V)) #check diagonals
+plot(acc_small$vi~diag(V))       
+str(acc_small)
+
+
+#cluster robust approach
+library(clubSandwich)
+V2<-impute_covariance_matrix(vi = acc_es$vi, cluster = acc_es$study, r = 0.7)
+V3<-impute_covariance_matrix(vi = acc_es$vi, cluster = acc_es$study, r = 0.3)
+
+all.equal(acc_es$vi, diag(V2)) #check diagonals
+length(acc_es$vi)
+length(diag(V2))
+plot(acc_es$vi~diag(V2))       
+
+
 
 #create variable for difference in acclimation temperature
 acc_es<- acc_es %>% 
   mutate(temp_diff= acclimation_temperature_2-acclimation_temperature_1)
+
+m1<-rma.mv(yi, V2, mods = ~temp_diff, data=acc_es)
+m2<-rma.mv(yi, V2, mods = ~temp_diff*temp_range, data=acc_es)
+m3<-rma.mv(yi, V2, mods = ~temp_diff*upper_lower, data=acc_es)
+m4<-rma.mv(yi, V2, mods = ~temp_diff+upper_lower, data=acc_es)
+
+coef_test(m1, vcov = "CR2", cluster = acc_es$study)
+coef_test(m2, vcov = "CR2", cluster = acc_es$study)
+coef_test(m3, vcov = "CR2", cluster = acc_es$study)
+coef_test(m4, vcov = "CR2", cluster = acc_es$study)
+
+n1<-rma.mv(yi, V3, mods = ~temp_diff, data=acc_es)
+n2<-rma.mv(yi, V3, mods = ~temp_diff*temp_range, data=acc_es)
+n3<-rma.mv(yi, V3, mods = ~temp_diff*upper_lower, data=acc_es)
+n4<-rma.mv(yi, V3, mods = ~temp_diff+upper_lower, data=acc_es)
+
+coef_test(n1, vcov = "CR2", cluster = acc_es$study)
+coef_test(n2, vcov = "CR2", cluster = acc_es$study)
+coef_test(n3, vcov = "CR2", cluster = acc_es$study)
+coef_test(n4, vcov = "CR2", cluster = acc_es$study)
 
 #run models
 full_mod<- rma.mv(yi, V, mods= ~temp_diff + ~thermal_limit_2 + ~temp_range + ~factor(phylum) + ~factor(ecosystem), 
                   slab = paste(study, sep = ""),
                   random = (~1|study), 
                   data = acc_es)
+
 mods<- dredge(full_mod, trace=2) #takes a look at all combinations of models that have a relative AIC value less than 2
 importance(mods) #not sure if other moderators are in this?
 
